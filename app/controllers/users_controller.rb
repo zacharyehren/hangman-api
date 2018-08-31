33bb6238e2c0a577f51c3bc8daba45d23b837d0b
Zachary Ehren <zachehren@gmail.com>
2018-08-31 12:26:42 -0700

    remove commented code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def authenticate
-    puts params[:username]
-    puts params[:password]
-
     user = User.find_by_username(params[:username])
     puts 'user:'
     puts user
@@ -10,12 +7,17 @@ class UsersController < ApplicationController
       puts 'User is nil'
       render json: "User not found with #{params[:username]}", status: :not_found
     else
-      render json: user, status: :ok
+      if user.authenticate(params[:password])
+        render json: user, status: :ok
+      else
+        puts "Bad password"
+        render json: "Password was incorrect!", status: :not_found
+      end
     end
   end
 
   def show
-    user = User.find_by_username(params[:username])
+    user = User.find(params[:id])
     render json: user
   end
 
@@ -40,6 +42,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:users).permit(:username, :password, :id, :wins, :losses, :total_games)
+    params.require(:users).permit(:username, :password, :id, :wins, :losses, :average, :total_games)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   validates :username, presence: true
+  has_secure_password
 end
